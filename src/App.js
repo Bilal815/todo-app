993c709a0e8f1c7a6be76035c7be713ae70768b0
Felipe Ferreira <felipedferreira11@gmail.com>
2020-08-15 22:06:47 -0400

    modal logic has changed a little

--- src/App.js
+++ src/App.js
@@ -4,30 +4,46 @@
 //  Online Deployment (Deployed Online)
 //  Database
 import React, { useState, useEffect } from 'react';
-import { Button, Input, InputLabel, FormControl } from '@material-ui/core';
+import { 
+  Button, 
+  Input, 
+  InputLabel, 
+  FormControl, 
+  Modal,
+  makeStyles
+} from '@material-ui/core';
+
+
 import Todo from './Todo'
 import './App.css';
 import db from './firebase';
-import firebase from 'firebase';
+import firebase from 'firebase'
 
-function App() {
-  const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState('');
 
-  // when the app loads, we need to listen to the database and fetch new todos as they get added/removed
-useEffect(() => {
-  // this code here... fires when the app.js loads
-  db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => 
-  {
-    setTodos(snapshot.docs.map(doc => (
-      {
-        ...doc.data(),
-        id: doc.id
-      }
-    )))
+const App = () => {
+
+  const [todos, setTodos] = useState([])
+  const [input, setInput] = useState('')
+  const [open, setOpen] = useState(false)
+  const [modalData, setModalData] = useState({
+    id: '',
+    input: ''
   })
 
-}, [])
+  // when the app loads, we need to listen to the database and fetch new todos as they get added/removed
+  useEffect(() => {
+    // this code here... fires when the app.js loads
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => 
+    {
+      setTodos(snapshot.docs.map(doc => (
+        {
+          ...doc.data(),
+          id: doc.id
+        }
+      )))
+    })
+
+  }, [])
 
 
   /**
@@ -61,18 +77,74 @@ useEffect(() => {
     })
   }
 
+  const openModal = (id) => {
+    db.collection('todos').doc(id).get().then((docRef) => {
+      setOpen(true)
+      /*
+        DOCUMENT EXAMPLE WILL LOOK LIKE THIS
+        --------------------------------------------------------
+        isCompleted: true
+        timestamp: t {seconds: 1597541101, nanoseconds: 48000000}
+        todo: "TEST"
+       */
+      const document = docRef.data()
+      setModalData({
+        id,
+        input: document.todo
+      })
+    })
+  }
+
 
   const renderTodoList = () => Array.isArray(todos) && todos.length > 0 
-    ? todos.map(todo => <Todo key={todo.id} todo={todo} />)
+    ? todos.map(todo => <Todo openModal={openModal} key={todo.id} todo={todo} />)
       : null
+      
+  const useStyles = makeStyles((theme) => ({
+        paper: {
+            position: 'absolute',
+            width: 400,
+            backgroundColor: theme.palette.background.paper,
+            border: '2px solid #000',
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3),
+        }
+  }))
+
+  const updateTodo = () => {
+      // update the todo with the new input text
+      db.collection('todos').doc(modalData.id).set({
+          todo: modalData.input,
+      }, { merge: true })
+      setOpen(false)
+  }
+
+  const writeNewModal = (e) => {
+    const input = e.target.value
+    const newModalState = {
+      ...modalData,
+      input
+    }
+    setModalData(newModalState)
+  }
+
 
   return (
     <div className="App">
+      <div id="my-modal">
+        <Modal style={{display: "flex", justifyContent: "center", alignItems: "center"}} open={open} onClose={e => setOpen(false)}>
+            <div className={useStyles().paper}>
+                <h1>Change Your TODO &#9989;</h1>
+                <input onChange={writeNewModal} value={modalData.input}/>
+                <Button onClick={updateTodo}>Update Todo</Button>
+            </div>
+        </Modal>
+      </div>
+
       <h1 className="capitalized">to-do list 📝</h1>
       <form>  
-        {/*<input />*/}
         <FormControl>
-          <InputLabel>Write To-Do &#9989;</InputLabel>
+          <InputLabel>Write To-Do ✔️</InputLabel>
           <Input value={input} onChange={e => {setInput(e.target.value)}}/>
         </FormControl>
         <Button type="submit" disabled={!input} variant="contained" color="primary" onClick={addTodo}>
@@ -85,7 +157,7 @@ useEffect(() => {
       </ul>
 
     </div>
-  );
+  )
 }
 
 export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -1,55 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Todo.css'
 import {
     List, 
     ListItem, 
-    // ListItemAvatar, 
     ListItemText, 
-    Button, 
-    Modal, 
     makeStyles
 } from '@material-ui/core'
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import db from './firebase';
 
-const useStyles = makeStyles((theme) => ({
-    paper: {
-        position: 'absolute',
-        width: 400,
-        backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
-    },
-    p: {
-        textDecoration: 'line-through',
-        opacity: '80%'
-    },
-}));
-
-
-
-
 
 const Todo = props => {
 
-    let { todo } = props
+    // Extracting the following info from props...Making it easier to reference the values...
+    let { todo, openModal } = props
     let { id, isCompleted } = todo
     todo = todo.todo
-
-
-    const [input, setInput] = useState();
-    const classes = useStyles();
-    const [open, setOpen] = useState(false)
-
-    const updateTodo = () => {
-        // update the todo with the new input text
-        db.collection('todos').doc(id).set({
-            todo: input,
-        }, { merge: true })
-        setOpen(false)
-        setInput('')
-    }
 
     const toggleIsCompleted = () => {
         db.collection('todos').doc(id).set({
@@ -61,34 +27,18 @@ const Todo = props => {
         db.collection('todos').doc(id).delete()
     }
 
-
-
     // Logic for dynamically applying the makeStyles-p-2 class which makes things be crossed out...
     let className = ''
     if(isCompleted) className += ' makeStyles-p-2'
 
-
-
-    return (
-            <>
-            <Modal open={open} onClose={e => setOpen(false)}>
-                <div className={classes.paper}>
-                    <h1>Change Your TODO &#9989;</h1>
-                    <input placeholder={props.todo.todo} value={input} onChange={e => setInput(e.target.value)}/>
-                    <Button  onClick={updateTodo}>Update Todo</Button>
-                </div>
-            </Modal>
-            <List className="todo__list">
+    return (<List className="todo__list">
                 <ListItem>
                     <ListItemText className={className} primary="Todos" secondary={props.todo.todo} />
                 </ListItem>
                 <button className={className} onClick={toggleIsCompleted}>&#9989;</button>
-                <button className={className} onClick={e => setOpen(true)}>&#9999;Edit</button>
+                <button className={className} onClick={() => openModal(id)}>&#9999;Edit</button>
                 <DeleteForeverIcon onClick={deleteForever}/>
-
-            </List>
-            </>
-    )
+            </List>)
 }
 
 export default Todo
