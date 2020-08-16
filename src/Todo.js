8d5615bd17195e11a4ccbf03089368fee3fe4d9e
Felipe Ferreira <felipedferreira11@gmail.com>
2020-08-15 22:39:53 -0400

    cross off list

--- src/Todo.js
+++ src/Todo.js
@@ -13,14 +13,12 @@ import db from './firebase';
 const Todo = props => {
 
     // Extracting the following info from props...Making it easier to reference the values...
-    let { todo, openModal } = props
+    let { todo, openModal, toggleIsCompleted } = props
     let { id, isCompleted } = todo
     todo = todo.todo
 
-    const toggleIsCompleted = () => {
-        db.collection('todos').doc(id).set({
-            isCompleted: !isCompleted
-        }, { merge: true })
+    const toggleCompleted = () => {
+        toggleIsCompleted(id, isCompleted)
     }
 
     const deleteForever = () => {
@@ -29,13 +27,13 @@ const Todo = props => {
 
     // Logic for dynamically applying the makeStyles-p-2 class which makes things be crossed out...
     let className = ''
-    if(isCompleted) className += ' makeStyles-p-2'
+    if(isCompleted) className += ' isComplete'
 
     return (<List className="todo__list">
                 <ListItem>
                     <ListItemText className={className} primary="Todos" secondary={props.todo.todo} />
                 </ListItem>
-                <button className={className} onClick={toggleIsCompleted}>&#9989;</button>
+                <button className={className} onClick={toggleCompleted}>&#9989;</button>
                 <button className={className} onClick={() => openModal(id)}>&#9999;Edit</button>
                 <DeleteForeverIcon onClick={deleteForever}/>
             </List>)
